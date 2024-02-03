833a12ac466acf436f4a21d547ea33224756c149
Angelo <mangelogm28@gmail.com>
2024-02-02 22:03:49 -0300

    Adiciona função para filtrar menções e links quando o tweet possui uma imagem ou vídeo
    Nesta alteração, foram feitas modificações nos seguintes arquivos:
    - package-lock.json
    - src/ai.request.ts
    - src/index.ts
    
    Essas mudanças visam melhorar a filtragem de conteúdo nos tweets que contêm imagens ou vídeos, removendo menções e links desnecessários.

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,80 @@
 import { aiRequest } from "./ai.request";
 import { client } from "./client";
 import { scraper } from "./scraper";
+import { Tweet } from "@the-convocation/twitter-scraper"
 
 async function observeProfile(username: string) {
 
-    console.log('Observing profile:', username);
     let lastTweetId: string | null | undefined = null;
+    let filteredText: string | undefined = '';
+    const targetedUserId: string = process.env.TARGETED_USER_ID || '';
+
+    console.log('Observing profile:', username);
 
     while (true) {
+        const tweet = await scraper.getLatestTweet(username, true, 1);
+
         console.log('Checking for new tweets...');
         console.log(new Date());
-        const tweet = await scraper.getLatestTweet(username, false, 1);
+
+        if (filteredText !== '') filteredText = ''
 
         if (tweet) {
-            if (tweet.id !== lastTweetId) {
+            if (tweet.id !== lastTweetId && tweet.userId === targetedUserId) {
                 lastTweetId = tweet.id;
-                aiRequest(tweet.text).then((response) => {
-                    if (response) {
-                        client.v2.reply(response, tweet.id);
-                    }
-                });
+
+                filteredText = filterText(tweet);
+                if (filterText !== undefined) {
+                    aiRequest(tweet.text).then((response) => {
+                        if (response) { client.v2.reply(response, tweet.id); }
+                    });
+                }
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, 60000)); // Check for new tweets every 60 seconds
+        }
+    }
+}
+
+function filterText(tweet: Tweet): string | undefined {
+
+    let formattedText: string = tweet.text ?? '';
+    if (formattedText === '') return undefined;
+
+    filterMentions();
+    filterLinks();
+    return formattedText;
+
+    function filterMentions() {
+        for (const mention of tweet.mentions) {
+            let regex: string = mention.username?.replace(/^/, "@") ?? '';
+
+            if (regex && tweet.text) {
+                const index: number = tweet.text.indexOf(regex);
+                if (index !== -1 && tweet.text[index + regex.length] === " ") {
+                    regex += ' ';
+                }
+
+                if (tweet.text?.includes(regex)) {
+                    formattedText = formattedText.replace(regex, '');
+                }
             }
         }
+    }
 
-        await new Promise((resolve) => setTimeout(resolve, 60000)); // Check for new tweets every 60 seconds
+    function filterLinks() {
+        const regex: RegExp = /\s?https:\/\/t\.co\/\S+/;
+        if (regex.test(formattedText)) {
+            formattedText = formattedText.replace(regex, '');
+        }
     }
 }
 
 async function scraperLogin(username: string | undefined, password: string | undefined): Promise<void> {
 
     try {
-        if (!username || !password) {
-            throw new Error("Username or password not provided");
-        }
+        if (!username || !password) throw new Error("Username or password not provided");
+
         await scraper.login(username, password);
         console.log("Logged in: " + await scraper.isLoggedIn());
     } catch (e) {
@@ -43,4 +85,4 @@ async function scraperLogin(username: string | undefined, password: string | und
 console.log('Starting bot...');
 console.log('Logging in...');
 scraperLogin(process.env.TWITTER_USERNAME, process.env.TWITTER_PASSWORD);
-observeProfile('apenasangelo');
+observeProfile('');
